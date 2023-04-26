4178705baa2977c551551a42b9111a386985d8a1
Ricardo Reyes Flores <ricardoreyesflores2016@outlook.com>
2023-04-26 11:40:30 -0600

    MODIFIED: juego.js - Se agrego la funcion pedirCarta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -8,20 +8,39 @@
 let deck = [];
 const tipos = ["C", "D", "H", "S"],
   especiales = ["A", "J", "Q", "K"];
+
+
+//Esta funcion crea una nueva baraja Mixeada
 const crearDeck = () => {
+    //Creando las cartas
   for (let i = 2; i <= 10; i++) {
     for (let tipo of tipos) {
       deck.push(i + tipo);
     }
   }
-
+  //Creando las Cartas especiales
   for (let tipo of tipos) {
     for (let especial of especiales) {
       deck.push(especial + tipo);
     }
   }
+  //Revolviendo el mazo
   deck = _.shuffle(deck);
   return deck;
 };
 
 crearDeck();
+
+
+//Pidiendo carta
+const pedirCarta = () => {
+    //Valida si hay cartas en el deck
+    if(deck.length === 0){
+        throw 'No hay Cartas en el Deck'
+    }
+    const carta = deck.pop();
+    return carta;
+}
+
+pedirCarta();
+
